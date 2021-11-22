d18c88a3713a76426a236fe94b9c2dd2c931c699
merjimenezc <merjimenezc@gmail.com>
2021-11-21 23:36:13 -0800

    completed db models + relationships. 1 importError

--- models.py
+++ models.py
@@ -1,23 +1,32 @@
-from flask import Flask, db
+from flask import Flask, render_template_string, redirect
 from datetime import datetime
+from flask_login import UserMixin, LoginManager, login_user, logout_user, FlaskLoginClient
 # from flaskblog import db, login_manager
-from flask_login import UserMixin
+from flask_blogging import db
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# db = SQLAlchemy(app)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# login = LoginManager(app)
+
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
+# @LoginManager.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
-    # identity column
     id = db.Column(db.Integer, primary_key=True)
-    # username column - unique so no same usernames
     username = db.Column(db.String(20), nullable=False, unique=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     # password column - 80 characters once hashed
     password = db.Column(db.String(80), nullable=False)
+    profile_image = db.Column(db.String(20), nullable=False, default='default.jpg')
     posts = db.relationship('Post', backref='author', lazy=True)
 
+# def __repr__(self):
+#         return f"User('{self.username}', '{self.email}', '{self.profile_image}')"
 
 def following(self):
         return (User
@@ -55,9 +64,17 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
+class Message(db.Model):
+    user = db.ForeignKeyField(User, backref='messages')
+    content = db.TextField()
+    pub_date = db.DateTimeField()
+
+# def __repr__(self):
+#         return f"Post('{self.title}', '{self.date_posted}')"
+
 def create_tables():
     with db:
-        db.create_tables([User, Relationship, Post])
+        db.create_tables([User, Relationship, Post, Message])
 
 if __name__ == '__main__':
     create_tables()
