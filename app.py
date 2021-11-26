a92a7b950ee8b92bb4cba8d1e6a6d1ad87c35ac1
RafaelEduardoLintao <relintao@gmail.com>
2021-11-25 22:43:31 -0800

    rough home page

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'user': 'ezxjams',
+        'content': 'wuss gud',
+    },
+    {
+        'user': 'user2',
+        'content': 'Happy Thanksgiving!',
+    },
+        {
+        'user': 'RLintao',
+        'content': 'Hello world!!',
+    }
+]
 
 @app.route('/')
 def index():
     return "<h1> Index </h1>"
+
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
