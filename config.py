fe5540d849b45c190204efa1a4fd3bbc82df7adc
tylerBrittain42 <tbrittain100@gmail.com>
2021-11-27 22:27:42 -0800

    migration completed

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'sewrjwao5rhiawllsdfkjlksenf,smdnvf,xzmcvn,zxcmvnsdfg'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///twoter.sqlite'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://ohyfcvzr:AkehyaoHTqP7U0sK0L_fe9SOwurIAfSK@kashin.db.elephantsql.com/ohyfcvzr'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
---
+++ load.py
@@ -0,0 +1,4 @@
+from app import db
+
+db.create_all()# # NEED ENROLLMENT
+db.session.commit()
