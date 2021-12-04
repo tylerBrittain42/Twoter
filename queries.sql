509a1aca6730067e311c1ee6013ab7d6768862a0
tbrittain2 <tbrittain2@ucmerced.edu>
2021-12-04 15:08:20 -0800

    implemented admin page

--- queries.sql
+++ queries.sql
@@ -12,3 +12,6 @@ WHERE follower_id=2
 2 results 
 9	my only tweet ahaha	2021-11-25 01:37:59.183238	3
 10	YESsssss	2021-11-25 01:38:18.546524	4
+
+INSERT INTO user(name,email,password,role, profile_image)
+VALUES('admin','none','pw','admin','none')
