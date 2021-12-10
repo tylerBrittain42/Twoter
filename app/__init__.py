5ad9de64d844826fb835b0a7dcbfbee48ef2116c
tbrittain2 <tbrittain2@ucmerced.edu>
2021-12-10 15:22:13 -0800

    added cors policy handling

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,12 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate, migrate
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
+CORS(app)
+# app.config['CORS_HEADERS'] = 'Content-Type'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
