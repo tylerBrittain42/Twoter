b6ce0ac60c04a5ccf0e6d861bf346b61f1bbb269
RafaelEduardoLintao <relintao@gmail.com>
2021-12-12 16:26:55 -0800

    page refresh on delete

--- app/static/feed-script.js
+++ app/static/feed-script.js
@@ -11,21 +11,21 @@ textCount.addEventListener("input", function () {
 });
 
 function deleteTwote(this_ele) {
-  if(confirm("Are you sure you want to delete this twowte?")){
-  console.log("delete clicked");
-  const ele = {
-    name: document.getElementById("twotecontent").id,
-  };
-  twote_id = this_ele.id;
-  fetch(`http://127.0.0.1:5000/twote?twote_id=${twote_id}`, {
-    method: "DELETE",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(ele),
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .then(() => location.reload());
-}
+  if(confirm("Are you sure you want to delete this twote?")){
+    console.log("delete clicked");
+    const ele = {
+      name: document.getElementById("twotecontent").id,
+    };
+    twote_id = this_ele.id;
+    fetch(`http://127.0.0.1:5000/twote?twote_id=${twote_id}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(ele),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      location.reload();
+  }
 }
 
 function cannotOpen(){
