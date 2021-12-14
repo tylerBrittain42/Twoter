1c7b7470327f6b6c852326adea092fa3d17840b4
tbrittain2 <tbrittain2@ucmerced.edu>
2021-12-13 16:08:51 -0800

    s

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'sewrjwao5rhiawllsdfkjlksenf,smdnvf,xzmcvn,zxcmvnsdfg'
+    SECRET_KEY = 'postgresql://ohyfcvzr:AkehyaoHTqP7U0sK0L_fe9SOwurIAfSK@kashin.db.elephantsql.com/ohyfcvzr'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CORS_HEADERS='Content-Type'
